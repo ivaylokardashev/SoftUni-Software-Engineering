e2ca34bbf8184286c70e50b440751ff71fa018e9
KirukaFukamo <ivaylo.kardashev@gmail.com>
2023-10-08 15:59:33 +0300

    Add a second solution of numbers_filter.py

---
+++ Python_Fundamentals/Lecture_Lists_Basics/numbers_filter_second_solution.py
@@ -0,0 +1,23 @@
+nums = int(input())
+
+COMMAND_EVEN = 'even'
+COMMAND_ODD = 'odd'
+COMMAND_POSITIVE = 'positive'
+COMMAND_NEGATIVE = 'negative'
+
+number_list = [int(input()) for x in range(nums)]
+
+command = input()
+filtered_list = []
+
+for element in number_list:
+    filtered_command = ((COMMAND_EVEN == command and element % 2 == 0) or
+                     (COMMAND_ODD == command and element % 2 != 0) or
+                     (COMMAND_POSITIVE == command and element >= 0) or
+                     (COMMAND_NEGATIVE == command and element < 0))
+
+    if filtered_command:
+        filtered_list.append(element)
+
+# print(number_list)
+print(filtered_list)
