823c22cc136e9d24cc2d4d3fefbe20af1b3bf175
KirukaFukamo <ivaylo.kardashev@gmail.com>
2023-11-15 14:58:14 +0200

    Solution of "Stock"
    Using method keys() program check is product in table_of_stock or not.

---
+++ Python-Fundamentals/Lecture/Dictionaries/stock.py
@@ -0,0 +1,13 @@
+items_to_unpacked = input().split()
+products_to_search = input().split()
+
+food = items_to_unpacked[::2]
+quantities = items_to_unpacked[1::2]
+table_of_stock = {f: int(q) for (f, q) in zip(food, quantities)}
+
+for product in products_to_search:
+    if product in table_of_stock.keys():
+        if table_of_stock[product] > 0:
+            print(f"We have {table_of_stock[product]} of {product} left")
+    else:
+        print(f"Sorry, we don't have {product}")
