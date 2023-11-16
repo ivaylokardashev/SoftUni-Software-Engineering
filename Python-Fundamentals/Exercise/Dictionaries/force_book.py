1ea743e2774b0afd541880979325bdcf31493f4a
KirukaFukamo <ivaylo.kardashev@gmail.com>
2023-11-16 12:47:48 +0200

    Update force_book.py
    Duplicated loops and conditions are optimized.

--- Python-Fundamentals/Exercise/Dictionaries/force_book.py
+++ Python-Fundamentals/Exercise/Dictionaries/force_book.py
@@ -9,24 +9,23 @@ while True:
     if "|" in command:
         command = command.split(" | ")
         force_side, force_user = command[0], command[1]
+
         if force_side not in force_book:
             force_book[f"{force_side}"] = []
-            for key, value in force_book.items():
-                if force_user in value:
-                    command = "This user exist"
-                    break
-            if command != "This user exist":
-                force_book[f"{force_side}"].append(force_user)
-        elif force_side in force_book:
-            for key, value in force_book.items():
-                if force_user in value:
-                    command = "This user exist"
-                    break
-            if command != "This user exist":
-                force_book[f"{force_side}"].append(force_user)
+
+        for key, value in force_book.items():
+            if force_user in value:
+                command = "This user exist"
+                break
+
+        if command != "This user exist":
+            force_book[f"{force_side}"].append(force_user)
     else:
         command = command.split(" -> ")
         force_side, force_user = command[1], command[0]
+
+        if force_side not in force_book:
+            force_book[f"{force_side}"] = []
 
         for key, value in force_book.items():
             if force_user in value:
@@ -38,13 +37,9 @@ while True:
                 if force_user in value:
                     user_index = value.index(force_user)
                     value.pop(user_index)
-                    if force_side not in force_book:
-                        force_book[f"{force_side}"] = []
                     force_book[f"{force_side}"].append(force_user)
                     break
         elif command != "This user exist":
-            if force_side not in force_book:
-                force_book[f"{force_side}"] = []
             force_book[f"{force_side}"].append(force_user)
 
         print(f"{force_user} joins the {force_side} side!")
